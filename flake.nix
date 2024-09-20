ad1a284fbcec92c12d5f2a593e881d5d63acd0d7
ev3nvy <12035264+ev3nvy@users.noreply.github.com>
2024-09-20 17:30:02 +0200

    feat(nix): add alejandra formatter for nix

--- flake.nix
+++ flake.nix
@@ -13,16 +13,25 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    alejandra = {
+      url = "github:kamadorueda/alejandra/3.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations = {
-      ev3nvy-desktop = nixpkgs.lib.nixosSystem {
+      ev3nvy-desktop = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux";
         specialArgs = {inherit inputs;};
         modules = [
           inputs.lanzaboote.nixosModules.lanzaboote
           ./nix/systems/ev3nvy-desktop
           inputs.home-manager.nixosModules.default
+          {
+            environment.systemPackages = [inputs.alejandra.defaultPackage.${system}];
+          }
         ];
       };
     };
