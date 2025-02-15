510a4e3a823ce06d6e05e99495f77264d62c9aee
ev3nvy <12035264+ev3nvy@users.noreply.github.com>
2025-02-15 23:21:25 +0100

    refactor(nix): move disk mounting to separate file

--- nix/systems/ev3nvy-desktop/default.nix
+++ nix/systems/ev3nvy-desktop/default.nix
@@ -6,6 +6,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./extra
     ../../shared/host.nix
     ../../modules/nixos/system/nvidia.nix
     ../../modules/nixos/programs
@@ -14,27 +15,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  fileSystems."/run/media/${username}/Other" = {
-    device = "/dev/disk/by-uuid/88006F63006F576A";
-    # required, otherwise disk is mounted as read-only
-    fsType = "ntfs-3g";
-    options = [
-      # From https://nixos.org/manual/nixos/stable/index.html#ch-file-systems:
-      # > System startup will fail if any of the filesystems fails to mount, dropping you to the
-      # > emergency shell. You can make a mount asynchronous and non-critical by adding
-      # > `options = [ "nofail" ];`.
-      "nofail"
-      # allows mounting this NTFS drive in R/W mode (use `id` to get `uid`);
-      # see https://nixos.wiki/wiki/NTFS
-      "rw"
-      "uid=1000"
-      # proper permissions (don't make everything executable);
-      # see https://github.com/NixOS/nixpkgs/issues/55807#issuecomment-463959647
-      "dmask=007"
-      "fmask=117"
-    ];
-  };
 
   # Enable networking
   networking.networkmanager.enable = true;
