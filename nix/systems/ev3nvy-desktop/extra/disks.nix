68121f9d811e7a0af8b29be1dffcfcd6822c03b7
ev3nvy <12035264+ev3nvy@users.noreply.github.com>
2025-02-19 14:20:15 +0100

    fix(nix): allow use of chmod/chown on mounted NTFS drive
    This isn't a perfect solution for dual-booted windows drives, but it's
    good enough for now. TODO comment mentions a way to improve this, but it
    requires me to get user id from windows or something, and I don't have
    time right now. Will do it eventually :^)

--- nix/systems/ev3nvy-desktop/extra/disks.nix
+++ nix/systems/ev3nvy-desktop/extra/disks.nix
@@ -12,11 +12,18 @@
       # allows mounting this NTFS drive in R/W mode (use `id` to get `uid`);
       # see https://nixos.wiki/wiki/NTFS
       "rw"
-      "uid=1000"
-      # proper permissions (don't make everything executable);
-      # see https://github.com/NixOS/nixpkgs/issues/55807#issuecomment-463959647
-      "dmask=007"
-      "fmask=117"
+      # allow use of chmod/chown on this NTFS drive;
+      # see: https://askubuntu.com/a/92866
+      # TODO: use `usermapping` instead;
+      #       see https://manpages.ubuntu.com/manpages/noble/en/man8/ntfs-3g.8.html#user%20mapping
+      "permissions"
+      # From https://manpages.ubuntu.com/manpages/noble/en/man8/ntfs-3g.8.html#options:
+      # This option prevents files, directories and extended attributes to be created with a name
+      # not allowed by windows, because
+      # - it contains some not allowed character,
+      # - or the last character is a space or a dot,
+      # - or the name is reserved.
+      "windows_names"
     ];
   };
 }
